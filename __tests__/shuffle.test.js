4133c48c6fa3d2d5ef1d11c91f077225b87dcef4
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-10-20 17:16:51 -0500

    finished testing the shuffle.js fuction

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,17 @@
 const shuffle = require("../src/shuffle");
 
-describe("shuffle should...", () => {
+let arr = [4,2,1,3,5,6]
+
+describe("shuffle should ..", () => {
   // CODE HERE
+  test('shuffle should be an array', () => {
+    let result = shuffle(arr)
+    expect( typeof result).toBe("object")
+
+    console.log(result)
+  });
+  
+  test('array should be the same length as argument passed in', () => {
+    expect(arr).toHaveLength(6)
+  })
 });
